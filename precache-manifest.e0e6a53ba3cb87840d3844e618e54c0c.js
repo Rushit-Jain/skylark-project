1f4c9c98838aa943feaaecd7f2d8869bcc3ec711
Rushit Jain <rushit07@gmail.com>
2020-12-26 01:37:15 +0530

    Updates

---
+++ precache-manifest.e0e6a53ba3cb87840d3844e618e54c0c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95be21076e8324af9bba1f48f96c65dc",
+    "url": "/skylark-project/index.html"
+  },
+  {
+    "revision": "29c33f2e484d5f61d8e2",
+    "url": "/skylark-project/static/css/2.81c1ac21.chunk.css"
+  },
+  {
+    "revision": "30a33aac0f8215deb2f7",
+    "url": "/skylark-project/static/css/main.11a0bf9d.chunk.css"
+  },
+  {
+    "revision": "29c33f2e484d5f61d8e2",
+    "url": "/skylark-project/static/js/2.4b8b9305.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/skylark-project/static/js/2.4b8b9305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30a33aac0f8215deb2f7",
+    "url": "/skylark-project/static/js/main.b322e9a3.chunk.js"
+  },
+  {
+    "revision": "c3c2a9f88f9b167d45ee",
+    "url": "/skylark-project/static/js/runtime-main.68cf147c.js"
+  },
+  {
+    "revision": "48ffe26b7efb50433fcd2d2f6835a622",
+    "url": "/skylark-project/static/media/BG Timeline1.48ffe26b.jpg"
+  },
+  {
+    "revision": "b0a8ea7e07b3376b85d19d921827dbb0",
+    "url": "/skylark-project/static/media/BG Timeline2.b0a8ea7e.png"
+  },
+  {
+    "revision": "76aeb17757134fec0e670a77f052e3de",
+    "url": "/skylark-project/static/media/BG7.76aeb177.png"
+  },
+  {
+    "revision": "d75ef8ef14cbfca85c8deee5ca682ce3",
+    "url": "/skylark-project/static/media/BG_HOME.d75ef8ef.jpeg"
+  },
+  {
+    "revision": "82025bbe0eac276b4f3f331ce0234d74",
+    "url": "/skylark-project/static/media/BW Skylark.82025bbe.png"
+  },
+  {
+    "revision": "03a6fc5e3be77cb2ffc0fbe928dcd206",
+    "url": "/skylark-project/static/media/BW Skylark3.03a6fc5e.png"
+  },
+  {
+    "revision": "2ec8557460d3a2cd7340b16ac84fce32",
+    "url": "/skylark-project/static/media/Raleway-Medium.2ec85574.ttf"
+  },
+  {
+    "revision": "a55ff2cd6e2cffc65817240e14da6813",
+    "url": "/skylark-project/static/media/Raleway-MediumItalic.a55ff2cd.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/skylark-project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/skylark-project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/skylark-project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/skylark-project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/skylark-project/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
